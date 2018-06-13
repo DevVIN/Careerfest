a5d59ba0482fe226c263f3b9a6bcffd7300ab415
vinodkanpurne <abc@xyz.com>
2018-06-13 21:43:38 +0530

    Commiting initial code structure

---
+++ Careerfest/Careerfest-Service/src/main/java/Com/careerfest/Careerfest_Service/App.java
@@ -0,0 +1,13 @@
+package Com.careerfest.Careerfest_Service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
